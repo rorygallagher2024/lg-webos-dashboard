db346ae8d7b07f9bf4e6608ec80a5e9ee3358fae
Rory Gallagher <rorygallagher2010@gmail.com>
2026-09-17 19:10:03 +0100

    Bump version to 0.36.0

--- server/tvweb.js
+++ server/tvweb.js
@@ -33,7 +33,7 @@ var zeroBuffer = MiniMQTT.zeroBuffer;
  * a link to /releases/tag/v<version>, so a value with no tag behind it gives a
  * 404 rather than a wrong page.
  */
-var TVWEB_VERSION = '0.35.2';
+var TVWEB_VERSION = '0.36.0';
 
 // ---------------------------------------------------------------- config
 var CONFIG = {
